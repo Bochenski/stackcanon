46631abc8b48941903262135cc5394194cbc0a6b
David Bochenski <David@Davids-MacBook-Air.local>
2011-08-03 16:22:57 +0100

    Ensuring that the first user to be created is granted sysadmin privileges

--- play/app/models/User.scala
+++ play/app/models/User.scala
@@ -8,7 +8,7 @@ import net.liftweb.json._
 import net.liftweb.json.JsonAST
 import net.liftweb.json.JsonDSL._
 
-class User(o: DBObject) extends DBInstance("User",o) {
+class User(o: DBObject) extends DBInstance("User", o) {
   lazy val oid = o.getAs[ObjectId]("_id")
   lazy val username = o.getAs[String]("username")
   lazy val first_name = o.getAs[String]("first_name")
@@ -22,7 +22,7 @@ class User(o: DBObject) extends DBInstance("User",o) {
 }
 
 object User extends DBBase[User]("Users") {
-  override def allXML = <Users>{ super.allXML }</Users>
+  override def allXML = <Users>{super.allXML}</Users>
 
   def login(username: String, password: String) = findOneBy(MongoDBObject("username" -> username, "password" -> Codec.hexMD5(password)))
 
@@ -47,7 +47,12 @@ object User extends DBBase[User]("Users") {
         builder += "password" -> Codec.hexMD5(password)
         builder += "isAdmin" -> isAdmin
         builder += "isStaff" -> isStaff
-        builder += "isSysAdmin" -> isSysAdmin
+        if (!isSysAdmin) {
+          builder += "isSysAdmin" -> checkForFirstUser()
+        }
+        else {
+          builder += "isSysAdmin" -> isSysAdmin
+        }
         builder += "google_open_id" -> google_open_id
         builder += "facebook_id" -> facebook_id
         val newObj = builder.result().asDBObject
@@ -58,9 +63,23 @@ object User extends DBBase[User]("Users") {
     }
   }
 
-  def create(username: String, first_name: String, surname: String, password: String, isAdmin: Boolean):Boolean =
-    create(username,first_name,surname,password,false,isAdmin,false, "", "")
 
+  def create(username: String, first_name: String, surname: String, password: String, isAdmin: Boolean): Boolean =
+    create(username, first_name, surname, password, false, isAdmin, false, "", "")
+
+  private var _hasUsers = false
+
+  private def checkForFirstUser() :Boolean = {
+    //called as each user is created, is this our first user, if so, make them a super user
+    if (!_hasUsers) {
+      if (coll.count == 0) {
+        Logger.info("making this user a sys admin")
+        _hasUsers = true
+        return true
+      }
+    }
+    return false
+  }
 
   def associateWithGoogleOpenID(user: User, id: String) {
     User.addField(user, "google_open_id", id)
@@ -82,10 +101,10 @@ object User extends DBBase[User]("Users") {
     update(user)
   }
 
-  def isSysAdmin(user :User) :Boolean = {
-     user.isSysAdmin match {
-       case None  => false
-       case Some(_) => user.isSysAdmin.get
-     }
+  def isSysAdmin(user: User): Boolean = {
+    user.isSysAdmin match {
+      case None => false
+      case Some(_) => user.isSysAdmin.get
+    }
   }
 }
